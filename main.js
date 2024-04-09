5d89d7862733b043e00da435236b87fa4a5de56e
{nonakamibuki} <mibukinonaka@gmail.com>
2024-04-09 20:50:07 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    $(window).scroll(function () {
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+  
+      $('.fadeIn').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+          $(this).addClass("is-fadein");
+        }
+      });
+    });
+
+    $(".hover-mask").on("click", function(){
+        $(this).toggleClass("is-hidden");
+        $(this).next().slideToggle(500);
+        $(".hover-mask").not($(this)).next().slideUp(700);
+    });
+
+    $(".js-input").keyup(function() {
+        if( $(this).val() ) {
+           $(this).addClass("not-empty");
+        } else {
+           $(this).removeClass("not-empty");
+        }
+      });
+
+  });
